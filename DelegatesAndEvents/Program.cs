bbc3e35a44f3a5947f8f4b6b5e976e56e547cde3
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-23 19:14:28 +0530

    [Ganesh] Add:Delegates and events file is added

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DelegatesAndEvents
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Delegates and events problem!");
+        }
+    }
+}
